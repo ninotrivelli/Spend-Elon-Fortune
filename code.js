d9c3cf4310deb4e8d2fc3e4dafef5171dcf4aead
Nicolas Bañales Trivelli <ninotrivelli@gmail.com>
2022-03-08 16:25:55 -0300

    items added

--- code.js
+++ code.js
@@ -237,14 +237,20 @@ function preLoad() {
     'https://i.imgur.com/uUyKgKG.jpg'
   );
   createAndSaveElement(
-    'Samsung S21 Ultra - 256GB',
-    1249,
-    'https://i.imgur.com/qebFEnn.jpg'
+    'Samsung S22 Ultra - 1TB',
+    1399,
+    'https://i.imgur.com/1Vf4DWy.jpg'
   );
   createAndSaveElement(
-    "New MacBook Pro 14' M1 Pro Top Spec",
-    3299,
+    "New MacBook Pro 14' M1 Max (64GB | 4TB) ",
+    4699,
     'https://i.imgur.com/69V42nb.jpg'
+  );
+
+  createAndSaveElement(
+    'New 2022 Mac Studio M1 Ultra (128GB | 8TB)',
+    6999,
+    'https://i.imgur.com/TaO01Rv.jpg'
   );
 
   createAndSaveElement(
